bfb010cddd39cb5f16ae93c7d3c30b684ce7641a
Shailesh Kumar <856shailesh@gmail.com>
2019-10-01 14:17:03 +0530

    1st Oct 1st commit

--- src/test/java/com/crm/qa/testcases/ContactsPageTest.java
+++ src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -3,18 +3,22 @@ package com.crm.qa.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.ContactPages;
 import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
 
 public class ContactsPageTest extends TestBase {
 	
 	LoginPage loginPage;
 	HomePage homePage;
 	ContactPages contactsPage;
+	
+	String sheetName="Contacts";
 
 	public ContactsPageTest() {
 		super();
@@ -34,10 +38,19 @@ public class ContactsPageTest extends TestBase {
 		Assert.assertTrue(contactsPage.verifyContactsLabel(), "Contatct Label Missing");
 	}
 	
-	@Test (priority=2)
-	public void validateCreateNewContact() {
+	@DataProvider
+	public Object[][] getCRMtestData() {
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}
+	
+	
+	
+	@Test (priority=2 , dataProvider = "getCRMtestData")
+	public void validateCreateNewContact(String fname, String lname) {
 		homePage.clickOnConatactsLink();
-		contactsPage.createNewContactByName("Mohandas", "Gandhi");
+		//contactsPage.createNewContactByName("Mohandas", "Gandhi");
+		contactsPage.createNewContactByName(fname, lname);
 	}
 	
 	
